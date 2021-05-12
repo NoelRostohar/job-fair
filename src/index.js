1274f583970c89dad769dc55f6b01d3d4210c9b9
NoelRostohar <noel.rostohar@arsfutura.co>
2021-05-12 21:07:44 +0200

    Clean Project

--- src/index.js
+++ src/index.js
@@ -1,15 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { ThemeProvider } from 'theme-ui';
 
 import App from './App';
-import theme from './ui/theme';
 
 ReactDOM.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <App />
-    </ThemeProvider>
+    <App />
   </React.StrictMode>,
   document.getElementById('root'),
 );
--- src/ui/theme/space.js
+++
@@ -1,1 +0,0 @@
-export default ['0px', '4px', '8px', '12px', '16px', '24px', '32px', '40px', '48px', '64px', '80px'];
