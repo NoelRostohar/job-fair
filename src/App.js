1eccc86e859eea1d0e65356d5d5d0b8ef8cf20ce
NoelRostohar <noel.rostohar@arsfutura.co>
2021-05-12 21:04:35 +0200

    Part 3

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
-import Sidebar from './components/Sidebar/Sidebar';
 import Feed from './components/Feed/Feed';
-import FloatingButton from './components/FloatingButton/FloatingButton';
 
 import FlexLayout from './ui/components/FlexLayout';
 
 const App = () => {
   return (
     <FlexLayout alignItems="flex-start" justifyContent="center">
-      <Sidebar />
       <Feed />
-      <FloatingButton />
     </FlexLayout>
   );
 };
